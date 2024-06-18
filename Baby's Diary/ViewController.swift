957f0f40672bd0a77313373e0cba778b5911d0e4
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-06-18 14:13:19 +0500

    Initial Commit

---
+++ Baby's Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baby's Diary
+//
+//  Created by Томирис Рахымжан on 18/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
