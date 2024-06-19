f791ed4ba46537d09c72aef0426ba11f9611835a
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-06-19 10:20:41 +0500

    Presenation page layout and constraints set

--- Baby's Diary/ViewController.swift
+++ Baby's Diary/ViewController.swift
@@ -9,11 +9,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "baby-icon"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hi there!"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ex."
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        label.textColor = .darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    lazy var labelsStackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [titleLabel, subTitleLabel])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .vertical
+        sv.alignment = .center
+        sv.spacing = 10
+        return sv
+    }()
+
+    lazy var mainStackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [imageView, labelsStackView])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .vertical
+        sv.alignment = .center
+        sv.spacing = 20
+        return sv
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
 
+    func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(mainStackView)
+    }
 
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            mainStackView.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20),
+            
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            imageView.widthAnchor.constraint(equalTo: mainStackView.widthAnchor),
+
+            labelsStackView.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            labelsStackView.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor)
+        ])
+    }
 }
-
